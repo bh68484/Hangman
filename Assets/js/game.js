a17868233a183fc0a89a1e7419d6cde02f02a0c5
Brandon Harrington <bh68484@gmail.com>
2018-02-04 16:44:29 -0500

    updated JS game files

--- Assets/js/game.js
+++ Assets/js/game.js
@@ -14,7 +14,6 @@ var guessesLeft = 10;
 
 
 //Functions
-gameStart();
 function gameStart () {
     randomWord = listOfWords[Math.floor(Math.random() * listOfWords.length)];
     individualLetters = randomWord.split("");
@@ -43,9 +42,60 @@ function gameStart () {
     console.log(individualLetters);
     console.log(wordBlanks);
     console.log(blankAndRightLetters);
-  
-    
-    
-    
+}
+//check letter exist in word
+function checkLetters (letter) {
+// check if letter exists in code at all
 
+var isLetterInWord = false;
+for (var i=0; i<wordBlanks; i++){
+    if (randomWord[i] == letter){
+        isLetterInWord = true;
+        // alert("letter");
+    }
+}
+//check where in the word letter exist - populate blank and success array
+if(isLetterInWord) {
+  for (var i=0; i<wordBlanks; i++) {
+    if(randomWord[i] == letter) {
+        blankAndRightLetters[i] = letter;
+        }
+    }  
+}
+
+//letter wasnt found
+else {
+    wrongGuesses.push(letter);
+    guessesLeft--
+}
+
+console.log(blankAndRightLetters);
+
+
+}
+
+function roundComplete() {
+    console.log("Win Count: " + wins + " | Loss Count: " + losses + " | Guesses Left" + guessesLeft);
+    //check if user won
+    if (wordLetters.toString() == blankAndRightLetters.toString()){
+        wins++
+        alert("You Won!")
+        document.getElementById("winCounter").innerHTML = wins;
+        gameStart();
+    }
+    //check if user lost
+}
+
+
+//Main Process
+//Initates the code
+
+
+gameStart();
+    document.onkeyup = function(event) {
+    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetters(letterGuessed);
+    roundComplete();
+// debugging
+    console.log(letterGuessed);
 }
